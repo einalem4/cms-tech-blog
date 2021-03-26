0e2c91334bd76a0d600bf2f00bb7c9293af8e185
einalem4 <melanie@codehangar.io>
2021-03-26 14:16:45 -0600

    set up front end fetch calls

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Comment extends Model {}
+class Comment extends Model { }
 
 // create fields/columns for comment model
 Comment.init({
